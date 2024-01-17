4aa5d21182fe0673352943f95ca45d311cbe8f04
D9fal <>
2024-01-17 16:42:55 +0000

    Add reset buttonto generate the ramdom numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,42 @@
 
 document.addEventListener("DOMContentLoaded", function () {
     let cpus = document.getElementsByClassName('cpu');
-    for (let i = 0; i < cpus.length; i++){
-        cpus[i].value =  Math.floor(Math.random() * 9) + 1;
-        console.log(cpus[i]);    
-    }
+    let btns = document.getElementsByClassName('btn');
+    let NberTryOut = 5 ; 
+    for (let  btn of  btns) {
+
+        btn.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "reset") {   
+                let NberTryOut = 5 ;                        
+                for (let i = 0; i < cpus.length; i++){
+                    cpus[i].value =  Math.floor(Math.random() * 9) + 1;
+                    
+                } 
+                
+            } 
+            else 
+            {
+                NberTryOut -= 1; 
+                if (NberTryOut === 0 ) {
+                    alert(`Awwww.... sorry you lost! click on reset to restart the game !`);
+                }
+                else
+                {
+                    checkNumber();
+                }
+                
+
+            }
+        });
+
     
+    }   
+
     
 });
 
+function checkNumber(){
+
+    console.log('checking the number');
+
+}
